11b7210a97e4ad37bcc2b1d6339dc9aa276acc74
YoungKrug <31873199+YoungKrug@users.noreply.github.com>
2024-04-26 11:45:14 -0400

    Fixed issue where opticluster was returning a matrix array and not a data.frame.

--- tests/testthat/test-test-opticluster.R
+++ tests/testthat/test-test-opticluster.R
@@ -2,7 +2,6 @@ test_that("Clustering returns proper results", {
   expected_df <- readRDS(here::here("tests/extdata/df_test_file.RDS"))
   matrix <- readRDS(here::here("tests/extdata/matrix_data.RDS"))
   df <- Opticluster::OptiCluster(matrix, 0.2, 2)
-  list <- c("matrix", "array")
-  expect_equal(class(df), list)
+  expect_equal(class(df), "data.frame")
   expect_equal(df, expected_df)
 })
