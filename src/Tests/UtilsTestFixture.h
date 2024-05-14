ec4fdaeeedadf0e19f6933a3b63191d4971307a6
YoungKrug <31873199+YoungKrug@users.noreply.github.com>
2024-05-14 18:02:09 -0400

    Utils command cluster and test work as expected!!

--- src/Tests/UtilsTestFixture.h
+++ src/Tests/UtilsTestFixture.h
@@ -14,24 +14,21 @@
 class UtilsTestFixture : TestFixture {
 public:
     bool TestMothurRandomShufflesRandomizesData(std::vector<int> listToRandomize,
-        const std::vector<int>& expectedResult);
+    bool expectedResult);
     bool TestMotherRandomShuffleOverloadRandomizesData(std::vector<std::string> listToRandomize,
-        const std::vector<std::string>& expectedResult);
+        bool expectedResult);
     bool TestGetNumNamesReturnsCorrectNames(const std::string& namesToGet, int expectedResult);
     bool TestGetOTUNamesReturnsCorrectNames(std::vector<std::string> currentLabels, int numBins,
         const std::string& tagHeader, int expectedResult);
     bool TestMothurConvertStringToIntIsCorrect(const std::string& item, int number, bool expectedResult);
     bool TestMothurConvertStringToFloatIsCorrect(const std::string& item, float number, bool expectedResult);
     bool TestMothurConvertStringToDoubleIsCorrect(const std::string& item, double number, bool expectedResult);
-    bool TestConvertWorksCorrectly(const std::string& str, bool failIfLeftoverChars);
+    bool TestConvertWorksCorrectly(const std::string& str, int expectedResult);
     bool TestGetSimpleLabelReturnsCorrectData(const std::string& label, const std::string& expectedResult);
     bool TestGetLabelTagsReturnsCorrectData(const std::string& label, const std::string& expectedResult);
     bool TestIsNumericIsCorrect(const std::string& stringToCheck, bool expectedResult);
-    bool TestSplitAtCommaReturnsCorrectData(const std::string& stringToSplit, std::vector<std::string> container);
-    bool TestSplitAtCommaOverloadReturnsCorrectData(std::string prefix, std::string suffix,
-        const std::string& expectedResultPrefix, const std::string& expectedResultSuffix);
-    bool TestSplitAtCommaIntOVerloadReturnsCorrectData(std::string stringToSplit, std::vector<int> container,
-        std::vector<int> containerResultExpected, std::string stringToSplitExpectedResult);
+    bool TestSplitAtCommaReturnsCorrectData(std::string& stringToSplit, std::vector<std::string> container,
+        const std::vector<std::string>& expectedResult);
 private:
     std::unique_ptr<Utils> utils;
     void Setup() override;
