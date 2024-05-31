9efe4360864fcf243e5480236c02d5379515b27f
YoungKrug <31873199+YoungKrug@users.noreply.github.com>
2024-05-31 16:40:48 -0400

    RMD Check is able to run successfully!

--- R/Cluster.R
+++ R/Cluster.R
@@ -9,19 +9,19 @@
 #' Detailed description of the function.
 #'
 #' @export
-#' @param param1 A Sparse Matrix.
-#' @param param2 A cutoff value
-#' @param param3 The number of iterations
+#' @param sparse_matrix A Sparse Matrix.
+#' @param cutoff A cutoff value
+#' @param iterations The number of iterations
 #' @return A data.frame of the clusters.
-OptiCluster <- function(sparse_matrix, cutoff, iterations)
-{
-  indexOneList <- sparse_matrix@i
-  indexTwoList <- sparse_matrix@j
-  valueList <- sparse_matrix@x
-  clustering_output_string <- MatrixToOpiMatrixCluster(indexOneList, indexTwoList, valueList, cutoff,
-                                   iterations)
-  df <- t(read.table(text = clustering_output_string, sep = "\t", header = TRUE))
-  df <- data.frame(df[-1,])
+opti_cluster <- function(sparse_matrix, cutoff, iterations) {
+  index_one_list <- sparse_matrix@i
+  index_two_list <- sparse_matrix@j
+  value_list <- sparse_matrix@x
+  clustering_output_string <- MatrixToOpiMatrixCluster(index_one_list, index_two_list, value_list, cutoff,
+                                                       iterations)
+  df <- t(read.table(text = clustering_output_string,
+                     sep = "\t", header = TRUE))
+  df <- data.frame(df[-1, ])
   colnames(df)[1] <- "cluster"
   return(df)
 }
--- tests/testthat/test-test-opticluster.R
+++ tests/testthat/test-test-opticluster.R
@@ -1,14 +1,7 @@
 test_that("Clustering returns proper results", {
-  expected_df <- readRDS(here::here("tests/extdata/df_test_file.RDS"))
-  matrix <- readRDS(here::here("tests/extdata/matrix_data.RDS"))
-  df <- Opticluster::OptiCluster(matrix, 0.2, 2)
-  # Cluster object
-  # Print objects
-  # Summary
-  # Cluster object makes abundance table + print/write statment
-  # Create test for all the c++ classes
-  # dotur
+  expected_df <- readRDS(test_path("extdata","df_test_file.RDS"))
+  matrix <- readRDS(test_path("extdata","matrix_data.RDS"))
+  df <- Opticluster::opti_cluster(matrix, 0.2, 2)
   expect_equal(class(df), "data.frame")
   expect_equal(df, expected_df)
 })
-
