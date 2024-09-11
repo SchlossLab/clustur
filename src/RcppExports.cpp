d15ab980c3d29451d781d4c3ff7341f7d5b83343
GregJohnsonJr <gregjohn456@gmail.com>
2024-09-11 15:27:25 -0400

    Modified RcppExports

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -85,9 +85,9 @@ END_RCPP
 RcppExport SEXP run_testthat_tests(SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_Opticluster_WritePhylipFile", (DL_FUNC) &_Opticluster_WritePhylipFile, 5},
-    {"_Opticluster_MatrixToOpiMatrixCluster", (DL_FUNC) &_Opticluster_MatrixToOpiMatrixCluster, 7},
-    {"_Opticluster_ClassicCluster", (DL_FUNC) &_Opticluster_ClassicCluster, 6},
+    {"_Opticluster_WritePhylipFile", (DL_FUNC) &_Opticluster_WritePhylipFile, 6},
+    {"_Opticluster_MatrixToOpiMatrixCluster", (DL_FUNC) &_Opticluster_MatrixToOpiMatrixCluster, 8},
+    {"_Opticluster_ClassicCluster", (DL_FUNC) &_Opticluster_ClassicCluster, 7},
     {"_Opticluster_start_profiler", (DL_FUNC) &_Opticluster_start_profiler, 1},
     {"_Opticluster_stop_profiler", (DL_FUNC) &_Opticluster_stop_profiler, 0},
     {"run_testthat_tests", (DL_FUNC) &run_testthat_tests, 1},
