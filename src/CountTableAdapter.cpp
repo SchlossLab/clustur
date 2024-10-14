b93d89a1aeeeb076306bf65e5a00e83e59bd352c
GregJohnsonJr <gregjohn456@gmail.com>
2024-10-14 17:53:36 -0400

    Removing extra storage files and merge issues

--- src/CountTableAdapter.cpp
+++ src/CountTableAdapter.cpp
@@ -116,12 +116,6 @@ std::string CountTableAdapter::GetNameByIndex(const int index) const {
     return sampleNames[index];
 }
 
-std::string CountTableAdapter::GetNameByIndex(const int index) const {
-    if(index > static_cast<int>(sampleNames.size()))
-        return "";
-    return sampleNames[index];
-}
-
 std::vector<std::string> CountTableAdapter::GetGroups() const {
     return groups;
 }
