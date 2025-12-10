89b867ded49867de953d80a1988b1cb08da7588e
GregJohnsonJr <gregjohn456@gmail.com>
2025-12-10 11:56:45 -0500

    Fix for compilation warnings

--- src/OptiData.cpp
+++ src/OptiData.cpp
@@ -61,22 +61,21 @@ std::string OptiData::getName(const long long index) {
 }
 
 bool OptiData::mccValidCalc() {
-    bool valid = true;
 
-    long long numSeqs = getNumSeqs();
-    double numDists = numSeqs * (numSeqs-1)/2;
+    const auto numSeqs = static_cast<double>(getNumSeqs());
+    const double numDists = numSeqs * (numSeqs-1)/2;
 
-    double totalClose = 0;
+    size_t totalClose = 0;
     //for each sequence (singletons removed on read)
-    for (int i = 0; i < closeness.size(); i++) {
-        totalClose += closeness[i].size();
+    for (const auto & closenessData : closeness) {
+        totalClose += closenessData.size();
     }
 
     // Inital setup of all singletons - badState <- TN == 0, FP == 0, FN == totalClose/2, TP = 0
     // Inital setup of one otu - badState <- TN == 0, FP == 0, FN == 0, TP = totalClose/2
-    if ((numDists - (totalClose/2)) == 0) {
+    if ((numDists - (static_cast<double>(totalClose)/2)) == 0) {
         return false;
     }
 
-    return valid;
+    return true;
 }
